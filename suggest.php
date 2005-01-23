4f2db3fb808f7e24f11714e5ce5651441bb85ac3
dlinsin <dlinsin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-01-23 21:35:40 +0000

    version 0.4 - released 05/01/16
    git-svn-id: https://plugins.svn.wordpress.org/suggest/trunk@565 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- suggest.php
+++ suggest.php
@@ -3,14 +3,14 @@
 	Plugin Name: suggest
 	Plugin URI: http://blog.linsin.de/index.php/archives/2004/12/23/wordpress-plugin-suggest/
 	Description: This plugin enables a feature like Google's suggest search. When typing characters in your blog search field suggestions are being presented.
-	Version: 0.3
+	Version: 0.4
 	Author: David Linsin
 	Author URI: http://blog.linsin.de/
 	*/
 
 /** CONFIGURATION */
 
-    global $suggest_path, $scriptPathSet, $MAX_SUGGESTIONS, $COLOR_MARKED_ROW, $COLOR_RESULTS, $COLOR_SUGGEST;
+    global $suggest_path, $scriptPathSet, $MAX_SUGGESTIONS, $COLOR_MARKED_ROW, $COLOR_RESULTS, $COLOR_SUGGEST, $RESULTS_IE_OUTPUT;
 
 	// change path to suggest plugin e.g. if your blog is situated in 'http://blog.linsin.de/wp' change path to '/wp/wp-content/plugins/suggest/'
     $suggest_path = "/wp-content/plugins/suggest/" ;
@@ -24,6 +24,8 @@
 	$COLOR_SUGGEST = "black";
 	// font of suggestions and results
 	$TEXT_FONT = "arial,sans-serif";
+	// enable or disable output of 'x results' in IE
+	$RESULTS_IE_OUTPUT = false;
 
 /** PLUGIN functions **/
 
@@ -37,9 +39,9 @@
 
     function insert_suggestLoad()
     {
-        global $suggest_path, $scriptPathSet, $COLOR_MARKED_ROW, $COLOR_RESULTS, $COLOR_SUGGEST, $TEXT_FONT;
+        global $suggest_path, $scriptPathSet, $COLOR_MARKED_ROW, $COLOR_RESULTS, $COLOR_SUGGEST, $TEXT_FONT, $RESULTS_IE_OUTPUT;
 		if($scriptPathSet) {?>
-        <script>InstallAC(document.searchform,document.searchform.s,document.searchform.subButton,"<?php echo $suggest_path; ?>suggest_data.php","en","","","",<?php echo "\"".$COLOR_MARKED_ROW."\", \"".$COLOR_RESULTS."\", \"".$COLOR_SUGGEST."\", \"".$TEXT_FONT."\"";?>);</script>
+        <script>InstallAC(document.searchform,document.searchform.s,document.searchform.subButton,"<?php echo $suggest_path; ?>suggest_data.php","en","","","",<?php echo "\"".$COLOR_MARKED_ROW."\", \"".$COLOR_RESULTS."\", \"".$COLOR_SUGGEST."\", \"".$TEXT_FONT."\", \"".$RESULTS_IE_OUTPUT."\"";?>);</script>
 		<?php
 			$scriptPathSet = false;
 		}
