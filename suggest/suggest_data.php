ea82b07e1769f17f2c8a855d3d5477bfef1e0eba
dlinsin <dlinsin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-01-23 21:36:16 +0000

    version 0.4 - released 05/01/16
    git-svn-id: https://plugins.svn.wordpress.org/suggest/tags/v0.4@566 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ suggest/suggest_data.php
@@ -0,0 +1,91 @@
+<?
+// David Linsin
+// version 0.4
+
+include('../../../wp-config.php');
+
+// WP 1.5 support
+if(empty($qu)) {
+$qu = $_REQUEST['qu'];
+}
+
+if(!empty($qu)) {
+	$query = "select distinct post_content as val from $tableposts where post_content like '%".$qu."%' order by val";
+
+	$db_values = $wpdb->get_results($query);
+	$result = "sendRPCDone(frameElement, \"".$qu."\", new Array(";
+	$i = 0;
+	$k = 0;
+	$exists = false;
+	$exit = false;
+	$previous;
+	foreach($db_values as $res) {
+		$text = $res->val;
+
+		// only words that starts with $qu e.g. Ju or ju or JU
+		preg_match_all('/(\b'.ucwords($qu).'[^[:space:]]*.)|(\b'.strtolower($qu).'[^[:space:]]*.)|(\b'.strtoupper($qu).'[^[:space:]]*.)/', $text, $match);
+		
+		// for all matched words of post
+		foreach($match[0] as $word) {
+			
+			// get rid of numbers and none word characters
+			preg_match('/[a-zA-Z]*/', $word, $word_match);
+			$word = $word_match[0];
+			
+			// check if already used
+			for($j = 0; $j < sizeof($previous); $j++) {
+				if($word == $previous[$j]) {
+					$exists = true;
+					break;
+				}
+			}
+	
+			// if text does not exist
+			if(!$exists) {
+				if($i == 0) {
+					$result = $result . "\"" . $word . "\"";
+				}
+				else {
+					$result = $result . ",\"" . $word . "\"";
+				}
+				$previous[$k] = $word;
+				$k++;
+			}
+			
+			$i++;
+			$exists = false;
+			
+			// exit if max suggestions reached
+			if($k >= $MAX_SUGGESTIONS) {
+				$exit = true;
+				break;
+			}
+		}
+		// if max suggestions reached exit
+		if($exit) {
+			break;
+		}
+	}
+	$result = $result . "), new Array(";
+
+	for($i=0; $i < sizeof($previous); $i++) {
+		$query = "select distinct count(*) as cnt from $tableposts where post_content like '%".addslashes($previous[$i])."%'";
+		$res = $wpdb->get_row($query);
+		$text = $res->cnt;
+		if($i == 0) {
+			$result = $result . "\"" . $text . "\"";
+		}
+		else {
+			$result = $result . ",\"" . $text . "\"";
+		}
+	}
+
+	$result = $result . "), new Array(\"\"));";
+	echo $result;
+}
+?>
+
+
+
+
+
